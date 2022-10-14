d8fbb8fae0165f9c9bacc3edda8ef86aa28df3f2
Shahzad Ansari <34826823+Shahzad-Ansari@users.noreply.github.com>
2022-10-13 22:46:12 -0500

    changed styleing
    added changes to jumbo tron such as image and styling

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import plotting_results as pr
 
 
 #https://dashcheatsheet.pythonanywhere.com/
+#https://getbootstrap.com/docs/4.0/utilities/colors/
 
 external_stylesheets = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css',
@@ -30,12 +31,43 @@ all_options = {
     '1000 Years': ['20% Budget Option', '40% Budget Option','60% Budget Option']
 }
 
+jumbotronStyle = {
+    
+    "position" : "fixed",
+    "background-color":"#92a8d1"    
+    
+    
+    
+}
 
+card = dbc.Card(
+    [
+        dbc.CardImg(
+            src="assets/incore-logo.png",
+            top=True,
+            style={"opacity": 1},
+        ),
+        
+    ],
+    style={
+            "width": "10rem",
+    },
+)
 
 jumbotron = html.Div(
     dbc.Container(
         [
-            html.H1("Seaborn IN-CORE Dashbord App", className="display-3"),
+            dbc.Row([
+                
+                dbc.Col(
+                    html.Div("Seaborn IN-CORE Dashbord App", className="display-5"),
+                    width = "auto"
+                ),
+                dbc.Col(card,width = {"size": 3, "order": "last", "offset": 3})
+            ],justify ="between"),
+            html.Br(),
+            html.Br(),
+            html.Br(),
             html.P(
                 "Data visualization dashboard for Seaborn. Model created by XXXXX Visualizations created by Shahzad Ansari",
                 className="lead",
@@ -45,44 +77,55 @@ jumbotron = html.Div(
         fluid=True,
         className="py-3",
     ),
-    className="p-3 bg-light rounded-3",
+    className="p-3 bg-dark text-light rounded-3",
+    #style = jumbotronStyle,
+    
 )
 
-app.layout = dbc.Container([
-    
-    jumbotron,
-    html.Hr(),
-    html.Div([html.H6("Select what budget and projected year you want to analyze")]),
-    
-    dbc.Row([
+other = html.Div(
+    dbc.Container([
         
-        dbc.Col(dcc.Dropdown(id='budget-radio',clearable=False)),
-        dbc.Col(
-            dcc.RadioItems(
-                list(all_options.keys()),
-                '500 Years',
-                id='year-radio',
-            )
-        ),
-    ]),
-    
-    
-    html.Hr(),
-    html.Div(id='repair'),
-    html.Div(id='dislocation'),
-    html.Div(id='eloss'),
-    html.Hr(),
-    dbc.Row([
-        dbc.Col([
-            html.Div(id = 'metricsNoSol'), 
-            html.Div(id = 'Yr500')   
+        html.Hr(),
+        html.Div([html.H6("Select what budget and projected year you want to analyze")]),   
+        dbc.Row([
+            
+            dbc.Col(dcc.Dropdown(id='budget-radio',clearable=False)),
+            dbc.Col(
+                dcc.RadioItems(
+                    list(all_options.keys()),
+                    '500 Years',
+                    id='year-radio',
+                )
+            ),
         ]),
-        dbc.Col(html.Div(html.H1("Second Table goes here")))
+        
+        
+        html.Hr(),
+        html.Div(id='repair'),
+        html.Div(id='dislocation'),
+        html.Div(id='eloss'),
+        html.Hr(),
+        dbc.Row([
+            dbc.Col([
+                html.Div(id = 'metricsNoSol'), 
+                html.Div(id = 'Yr500')   
+            ]),
+            dbc.Col(html.Div(html.H1("Second Table goes here")))
+            
+        ])
         
     ])
     
     
-],fluid=False)
+    
+)
+
+
+
+app.layout = dbc.Container([
+    jumbotron,
+    other 
+],fluid=True)
 
 @app.callback(
     Output('budget-radio', 'options'),
